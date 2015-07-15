8059e6e473fd7658b9b50266477715926da5db8b
bobby kaz <robert.kazimiroff@gmail.com>
2015-07-15 00:08:23 -0700

    Everything setup and migrated out of phone app sync project

---
+++ Fitbit.TestConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitbit.Developer.Secrets;
+using Fitbit.API.Model.Sleep;
+using Fitbit.API.Model.Activities;
+using Fitbit.API.Model.Common.Enums.Activities;
+
+
+namespace Fitbit.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSleep();
+            Console.WriteLine("Done");
+        }
+
+        public static void TestSleep()
+        {
+            Fitbit.API.Client.FitbitClient client = new Fitbit.API.Client.FitbitClient(Secrets.ClientID, Secrets.ClientConsumerSecret, Secrets.CallbackURI);
+            client.SetBearerAuthorizationHeader(Secrets.TestAccessToken);
+            DateTime sampleDay = DateTime.Now;
+
+            LogSleepRequest req = new LogSleepRequest() { startTime = DateTime.Now, date = DateTime.Now, duration = 600000 };
+
+            var sleepLog = client.LogSleep(req).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var sleeps = client.GetSleep(req.date).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            client.DeleteSleep(sleepLog.sleep.logId).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        public static void TestActivity()
+        {
+            Fitbit.API.Client.FitbitClient client = new Fitbit.API.Client.FitbitClient(Secrets.ClientID, Secrets.ClientConsumerSecret, Secrets.CallbackURI);
+            client.SetBearerAuthorizationHeader(Secrets.TestAccessToken);
+
+            DateTime sampleDay = DateTime.Now;
+
+            var availableActivities = client.GetAvailableActivities().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var walkActivity = availableActivities.categories.Where(c => c.name == "Walking").FirstOrDefault();
+            int targetId = walkActivity.id;
+
+            PostActivityRequest newAct = new PostActivityRequest()
+            {
+                activityId = 17151,//90013,
+                distance = 98,
+                distanceUnit = DistanceUnit.Steps,
+                durationMillis = 120000,
+                startTime = DateTime.Now
+            };
+
+            var myActivites = client.GetUserActivityLogs(sampleDay).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var updatedLog = client.UpdateActivityLog(myActivites.activities.FirstOrDefault().logId, newAct).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            client.DeleteActivityLog(updatedLog.activityLog.logId).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var newAccLog = client.CreateActivityLog(newAct).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            client.DeleteActivityLog(newAccLog.activityLog.logId).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var user = client.GetUser().ConfigureAwait(false).GetAwaiter().GetResult();
+
+        }
+
+    }
+}
