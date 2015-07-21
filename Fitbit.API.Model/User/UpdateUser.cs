2d3aaa251d440b9aced5a15683091f9657361d37
bobby kaz <robert.kazimiroff@gmail.com>
2015-07-20 22:34:45 -0700

    Renamed, moved updateUser request

--- Fitbit.API.Model/User/UpdateUser.cs
+++ Fitbit.API.Model/User/UpdateUser.cs
@@ -9,42 +9,42 @@ using Fitbit.API.Model.Common.Enums.Profile;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
+
 namespace Fitbit.API.Model.User
 {
-    public class User
+    public class UpdateUserRequest
     {
         public string fullName { get; set; }
         public string displayName { get; set; }
         public string nickname { get; set; }
 
-        [JsonConverter(typeof(StringEnumConverter))] 
-        public Gender gender { get; set; }
-        
-        public DateTime dateOfBirth { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Gender? gender { get; set; }
+
+        [JsonIgnore]
+        public DateTime? dateOfBirth { get; set; }
+
+        public string dateOfBirthString
+        {
+            get { return dateOfBirth.HasValue ? string.Format("{0:yyyy-MM-dd}", dateOfBirth) : null; }
+        }
+
         public string aboutMe { get; set; }
 
-        public string encodedId { get; set; }
-
-        public string avatar { get; set; }
-        public string avatar150 { get; set; }
-        
         public string city { get; set; }
         public string country { get; set; }
         public string state { get; set; }
         public string timezone { get; set; }
-        public int offsetFromUTCMillis { get; set; }
-        
+
         [JsonConverter(typeof(StringEnumConverter))]
-        public DayOfWeek startDayOfWeek { get; set; }
+        public DayOfWeek? startDayOfWeek { get; set; }
 
-        public DateTime memberSince { get; set; }
+        public double? height { get; set; }
+        public double? weight { get; set; }
+        public double? strideLengthRunning { get; set; }
+        public double? strideLengthWalking { get; set; }
 
-        public double height { get; set; }
-        public double weight { get; set; }
-        public double strideLengthRunning { get; set; }
-        public double strideLengthWalking { get; set; }
-
-        [JsonConverter(typeof(StringEnumConverter))] 
+        [JsonConverter(typeof(StringEnumConverter))]
         public Locale? locale { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         public Locale? distanceUnit { get; set; }
@@ -56,10 +56,5 @@ namespace Fitbit.API.Model.User
         public Locale? waterUnit { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         public Locale? glucoseUnit { get; set; }
-    }
-
-    public class GetUserResponse
-    {
-        public User user { get; set; }
     }
 }
